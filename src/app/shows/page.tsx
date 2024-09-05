893356795324f351b9ab4bb1df9e4b099a27b358
Darren <darrennathaneil@gmail.com>
2024-09-05 17:32:26 +0700

    fix deploy error

--- src/app/shows/page.tsx
+++ src/app/shows/page.tsx
@@ -1,9 +1,9 @@
-import TopRated from "@/components/TopRatedPage/TopRated";
+import TopRated from "@/components/TopRatedPage";
 
 export default function ShowsPage() {
     return (
         <div className="flex items-center flex-col max-w-screen-2xl mx-auto">
-            <TopRated isMovie={false}/>
+            <TopRated isMovie={false} />
         </div>
     );
 }
